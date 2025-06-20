70759e0442720b180caa0448983cdcb976f82b8e
Jessie <jessie.2006.14@gmail.com>
2025-06-19 20:04:05 -0700

    Add Data of the products

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.querySelectorAll('.Products').forEach(product =>{
         window.location.href = 'product.html'
     })
 })
-// Selecte the items to display them in the products page 
+// Select the items to display them in the products page 
     const productJSON = localStorage.getItem('selectedProduct')
         const product = JSON.parse(productJSON);
         document.getElementById('product-name').textContent = product.name;
